ddeda14e5cbfacc40d74d6bf95a438966f26f6b7
Maarten Bressinck <maartenbressinck@MacBook-Pro-van-Maarten.local>
2014-03-18 14:20:08 +0100

    new test

---
+++ mover/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/classes.dex : \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/classes/be/devine/mover/BuildConfig.class \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/classes/be/devine/mover/mover.class \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/classes/be/devine/mover/R$attr.class \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/classes/be/devine/mover/R$drawable.class \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/classes/be/devine/mover/R$string.class \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/classes/be/devine/mover/R$xml.class \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/classes/be/devine/mover/R.class \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/dexedLibs/classes-2ab8faf48fa2d8a013db4b5df1b86b42.jar \
---
+++ mover/platforms/android/ant-build/mover.ap_.d
@@ -0,0 +1,25 @@
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/mover.ap_ \
+ : /Users/maartenbressinck/watmLaunch/mover/platforms/android/res/drawable/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/res/drawable-hdpi/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/res/drawable-ldpi/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/res/drawable-mdpi/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/res/values/strings.xml \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/res/xml/config.xml \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/res/drawable/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/_where-is-www.txt \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/cordova.js \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/cordova_plugins.js \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/css \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/img \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/index.html \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/js \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/css/index.css \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/img/logo.png \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/assets/www/js/index.js \
+/Users/maartenbressinck/watmLaunch/mover/platforms/android/ant-build/AndroidManifest.xml \
